13dd532adb95de3fa9292da5de42658e7c021c5e
Li Yi Yu <li@posthog.com>
2023-09-14 01:11:58 -0400

    more tests

--- site.test.js
+++ site.test.js
@@ -1,9 +1,11 @@
-import { createShadow, callSurveys } from './site';
+import { createShadow, callSurveys, inject } from './site'
 
-describe('my test suite', () => {
+describe('survey display logic', () => {
     afterEach(() => {
         // we have to manually reset the DOM after each test
-        document.getElementsByTagName('html')[0].innerHTML = ''; 
+        document.getElementsByTagName('html')[0].innerHTML = ''
+        localStorage.clear()
+        jest.clearAllMocks()
     })
 
     test('createShadow', () => {
@@ -11,9 +13,11 @@ describe('my test suite', () => {
         const mockShadow = createShadow(`.survey-${surveyId}-form {}`, surveyId)
         expect(mockShadow.mode).toBe('open')
         expect(mockShadow.host.className).toBe(`PostHogSurvey${surveyId}`)
-    });
+    })
 
-    const mockSurveys = [{ id: 'testSurvey1', name: 'Test survey 1', appearance: null, questions: [{ question: 'How satisfied are you with our newest product?', description: 'This is a question description', type: 'rating', display: 'number', scale: 10, lower_bound_label: 'Not Satisfied', upper_bound_label: 'Very Satisfied' }] }]
+    let mockSurveys = [
+        { id: 'testSurvey1', name: 'Test survey 1', appearance: null, questions: [{ question: 'How satisfied are you with our newest product?', description: 'This is a question description', type: 'rating', display: 'number', scale: 10, lower_bound_label: 'Not Satisfied', upper_bound_label: 'Very Satisfied' }] },
+    ]
     const mockPostHog = {
         getActiveMatchingSurveys: jest.fn().mockImplementation((callback) => callback(mockSurveys)),
         get_session_replay_url: jest.fn(),
@@ -28,28 +32,62 @@ describe('my test suite', () => {
 
         // now we dismiss the survey
         const cancelButton = document.getElementsByClassName(`PostHogSurvey${mockSurveys[0].id}`)[0].shadowRoot.querySelectorAll('.form-cancel')[0]
-        cancelButton.click();
+        cancelButton.click()
         expect(mockPostHog.capture).toBeCalledTimes(2)
         expect(mockPostHog.capture).toBeCalledWith('survey dismissed',  {$survey_id: "testSurvey1", $survey_name: "Test survey 1", sessionRecordingUrl: undefined })
         expect(localStorage.getItem(`seenSurvey_${mockSurveys[0].id}`)).toBe('true')
 
-        // now we clear the DOM and call surveys again, and it should not show the survey
-        document.getElementsByTagName('html')[0].innerHTML = ''; 
+        // now we clear the DOM to imitate a new page load and call surveys again, and it should not show the survey
+        document.getElementsByTagName('html')[0].innerHTML = ''
         callSurveys(mockPostHog, false)
         expect(document.getElementsByClassName(`PostHogSurvey${mockSurveys[0].id}`)[0]).toBe(undefined)
-        // no capture events are called because the survey is not shown
+        // no additional capture events are called because the survey is not shown
         expect(mockPostHog.capture).toBeCalledTimes(2)
     })
-})
 
-test('does not show survey to user if they have already completed it', () => {
-})
+    test('does not show survey to user if they have already completed it', () => {
+        expect(localStorage.getItem(`seenSurvey_${mockSurveys[0].id}`)).toBe(null)
+        callSurveys(mockPostHog, false)
+        expect(mockPostHog.capture).toBeCalledTimes(1)
+        expect(mockPostHog.capture).toBeCalledWith('survey shown', {$survey_id: "testSurvey1", $survey_name: "Test survey 1", sessionRecordingUrl: undefined })
 
-test('does not show survey to user if they have seen it before and survey wait period is set', () => {
-})
+        // submit the survey
+        const submitButton = document.getElementsByClassName(`PostHogSurvey${mockSurveys[0].id}`)[0].shadowRoot.querySelectorAll('.rating_1')[0]
+        submitButton.click()
+        expect(mockPostHog.capture).toBeCalledTimes(2)
+        expect(mockPostHog.capture).toBeCalledWith('survey sent',  {$survey_id: "testSurvey1", $survey_name: "Test survey 1", $survey_question: 'How satisfied are you with our newest product?', $survey_response: 1, sessionRecordingUrl: undefined })
+        expect(localStorage.getItem(`seenSurvey_${mockSurveys[0].id}`)).toBe('true')
 
-test('callSurveys runs in an interval', () => {
-})
+        // now we clear the DOM to imitate a new page load and call surveys again, and it should not show the survey
+        document.getElementsByTagName('html')[0].innerHTML = ''
+        callSurveys(mockPostHog, false)
+        expect(document.getElementsByClassName(`PostHogSurvey${mockSurveys[0].id}`)[0]).toBe(undefined)
+        // no additional capture events are called because the survey is not shown
+        expect(mockPostHog.capture).toBeCalledTimes(2)
+    })
 
-test('creates open text and link type surveys', () => {
+    test('does not show survey to user if they have seen it before and survey wait period is set', () => {
+        mockSurveys = [{ id: 'testSurvey2', name: 'Test survey 2', appearance: null, conditions: { seenSurveyWaitPeriodInDays: 10}, questions: [{ question: 'How was your experience?', description: 'This is a question description', type: 'rating', display: 'emoji', scale: 5, lower_bound_label: 'Not Good', upper_bound_label: 'Very Good' }] }]
+        expect(mockSurveys[0].conditions.seenSurveyWaitPeriodInDays).toBe(10)
+        expect(localStorage.getItem(`seenSurvey_${mockSurveys[0].id}`)).toBe(null)
+        callSurveys(mockPostHog, false)
+        expect(mockPostHog.capture).toBeCalledTimes(1)
+        expect(mockPostHog.capture).toBeCalledWith('survey shown', {$survey_id: "testSurvey2", $survey_name: "Test survey 2", sessionRecordingUrl: undefined })
+        expect(localStorage.getItem('lastSeenSurveyDate').split('T')[0]).toBe(new Date().toISOString().split('T')[0])
+
+        document.getElementsByTagName('html')[0].innerHTML = ''
+        callSurveys(mockPostHog, false)
+        expect(document.getElementsByClassName(`PostHogSurvey${mockSurveys[0].id}`)[0]).toBe(undefined)
+        // no additional capture events are called because the survey is not shown
+        expect(mockPostHog.capture).toBeCalledTimes(1)
+    })
+
+    test('when url changes, callSurveys runs again', () => {
+        jest.useFakeTimers()
+        jest.spyOn(global, 'setInterval')
+        const mockConfig = {}
+        inject({ config: mockConfig, posthog: mockPostHog })
+        expect(mockPostHog.getActiveMatchingSurveys).toBeCalledTimes(1)
+        expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1500)
+    })
 })
