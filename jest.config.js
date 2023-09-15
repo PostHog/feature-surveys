397abb9d631083c481b2bb9d849938add20116bd
Li Yi Yu <li@posthog.com>
2023-09-15 11:16:06 -0400

    Tests and CI (#16)
    * tests wip
    
    * first test successful
    
    * fix site.ts
    
    * lock file
    
    * remove console log
    
    * more tests
    
    * CI
    
    * use yarn
    
    * add typescript

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    preset: 'ts-jest',
+    moduleDirectories: ['node_modules', 'src'],
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    transformIgnorePatterns: [],
+    globals: {
+        'ts-jest': {
+            // Without isolatedModules, tests run realy slow, so we enable them
+            isolatedModules: true,
+        },
+    },
+    testEnvironment: "jest-environment-jsdom",
+}
